51eb2c3c85a9bec7e89aaa91ce947a429dbecb4c
Dabian Garnica <DabianExtras@gmail.com>
2025-10-27 22:48:26 -0400

    initial commit

---
+++ BlockByBlock/BlockByBlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlockByBlockApp.swift
+//  BlockByBlock
+//
+//  Created by Dabian  on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct BlockByBlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
