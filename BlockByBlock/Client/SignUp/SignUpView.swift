84a708dd34a0f89a82bf69b4081c0440a570a206
Richard Brito <richardbrito116@gmail.com>
2025-11-17 14:26:58 -0500

    Add login and signup views

---
+++ BlockByBlock/Client/SignUp/SignUpView.swift
@@ -0,0 +1,85 @@
+//
+//  SignUpView.swift
+//  BlockByBlock
+//
+//  Created by Richard Brito on 11/16/25.
+//
+
+import SwiftUI
+
+struct SignUpView: View {
+    @State private var fullName: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+
+    var body: some View {
+        VStack(spacing: 40) {
+            Spacer()
+
+            Text("Block by Block")
+            // Need to import mojangles.otf into xcode and put it here. will finish later
+                .font(.custom("PressStart2P-Regular", size: 28))
+                .multilineTextAlignment(.center)
+                .padding(.top, 40)
+
+            VStack(spacing: 32) {
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Name")
+                        .font(.system(size: 18, weight: .medium))
+                    TextField("FirstName LastName", text: $fullName)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
+                }
+
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Email")
+                        .font(.system(size: 18, weight: .medium))
+                    TextField("Email", text: $email)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
+                }
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Password")
+                        .font(.system(size: 18, weight: .medium))
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
+                }
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Confirm Password")
+                        .font(.system(size: 18, weight: .medium))
+                    SecureField("Confirm Password", text: $confirmPassword)
+                        .padding()
+                        .background(Color(.systemGray6))
+                        .cornerRadius(12)
+                }
+
+
+                Button(action: {}) {
+                    Text("Sign Up")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color(.systemGray4))
+                        .foregroundColor(.black)
+                        .cornerRadius(12)
+                }
+            }
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .stroke(Color(.systemGray4), lineWidth: 2)
+            )
+            .padding(.horizontal, 24)
+
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    SignUpView()
+}
